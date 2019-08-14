01a0f8571b0388cdb8c3ba19758fc0464966d4c3
Chris Grandin <cgrandin@shaw.ca>
2019-08-14 08:35:53 -0700

    Remove space from text so french works

--- R/figures.R
+++ R/figures.R
@@ -363,7 +363,7 @@ plot_proj_biomass_density <- function(models,
               alpha = ribbon_alpha,
               color = "transparent",
               fill = "red") +
-    labs(x = paste0(en2fr("Spawning biomass ", translate), yr, " (1,000 t)"),
+    labs(x = paste0(en2fr("Spawning biomass", translate), yr, " (1,000 t)"),
          y = en2fr("Density", translate)) +
     facet_wrap(~ region, ncol = 2, dir = "v", scales = "free")
   g
