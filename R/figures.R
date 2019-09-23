75662b0d55920f95620acfca3c5bf16006389cbf
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2019-09-23 10:06:16 -0700

    Add space

--- R/figures.R
+++ R/figures.R
@@ -369,7 +369,7 @@ plot_proj_biomass_density <- function(models,
               alpha = ribbon_alpha,
               color = "transparent",
               fill = "red") +
-    labs(x = paste0(en2fr("Spawning biomass", translate), yr, " (1,000 t)"),
+    labs(x = paste0(en2fr("Spawning biomass", translate), " ", yr, " (1000 t)"),
          y = en2fr("Density", translate)) +
     facet_wrap(~ region, ncol = 2, dir = "v", scales = "free_y")
   g
