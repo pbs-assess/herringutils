3a0a2c8a4c4983918feb540cd6e7d6dc904859af
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-08-17 13:49:17 -0700

    Revert "Number, not in thousands"
    This reverts commit 7ffb2c1c9313d14d1cfe55b558f255fa5fca963d.

--- R/figures.R
+++ R/figures.R
@@ -46,6 +46,8 @@ plot_catch <- function(df,
 plot_ic <- function(df,
                     xlim = c(NA, NA),
                     translate = FALSE) {
+  df <- df %>%
+    mutate(Number = Number / 1000)
   if(translate){
     df <- df %>%
       mutate(Type = en2fr(Type, translate, case = "sentence"))
@@ -57,7 +59,8 @@ plot_ic <- function(df,
   g <- ggplot(data = df, mapping = aes(x = Year, y = Number, fill = Type)) +
     geom_col(position = "dodge") +
     labs(x = en2fr("Year", translate, case = "sentence"),
-         y = en2fr("Number of fish", translate, case = "sentence"),
+         y = paste0(en2fr("Number of fish", translate, case = "sentence"),
+                    " (x 1,000)"),
          fill = en2fr("Type", translate, case = "sentence")) +
     scale_fill_viridis_d() +
     scale_y_continuous(labels = comma) +
